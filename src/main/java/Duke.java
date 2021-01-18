b1f49bd439d119e514be69bed471578035187bfc
Jia Qing <jiaqing2303@gmail.com>
2021-01-19 01:56:03 +0800

    Add initial skeletal

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
 public class Duke {
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String hline = "_______________________________________________________";
+        String greetString = " Hello! I'm Duke\n" + " What can I do for you?\n" + hline;
+        String exitString = " Bye. Hope to see you again soon!\n" + hline;
+
+        System.out.println(hline);
+        System.out.println(logo + greetString);
+
+        /* Main Code */
+
+        System.out.println(exitString);
     }
 }
